58c5db37a07c0e316c89b0fe4b1193911ef65940
zhang <13051885033@163.com>
2019-04-26 06:52:13 +0000

    batch size设置为32

--- config.py
+++ config.py
@@ -1,13 +1,14 @@
+
 img_size = 96
 # img_size = 28
 channels = 3
 # channels = 1
 img_shape = (channels, img_size, img_size)
-batch_size = 128
+batch_size = 64
 latent_dim = 128
 lr = 5e-5
 epoch_num = 1000
-n_critic = 5
+n_critic = 10
 n_epochs = 2000
 clip_value = 0.01
 sample_interval = 200
